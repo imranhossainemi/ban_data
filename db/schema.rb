d8e953d3727202c92e9025589488fb26766d9044
Imran Hossain <blur-imran@hotmail.com>
2024-05-22 20:52:50 +0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_21_154509) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_21_232025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "abandon_data", force: :cascade do |t|
-    t.integer "steam_id"
     t.date "abandon_date"
     t.time "abandon_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "steam_id_id", null: false
+    t.index ["steam_id_id"], name: "index_abandon_data_on_steam_id_id"
   end
 
+  create_table "steam_ids", force: :cascade do |t|
+    t.string "steam_name"
+    t.integer "steam_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "abandon_data", "steam_ids"
 end
